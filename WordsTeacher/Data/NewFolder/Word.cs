0217ef07ea89b70731b57c708cb2295d27a20156
Leghuir <qwerty1214@gmail.com>
2020-05-02 13:42:15 +0200

    Add project files.

---
+++ WordsTeacher/Data/NewFolder/Word.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordsTeacher.Data.NewFolder
+{
+    public class Word
+    {
+    }
+}
