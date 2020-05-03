5812a355f14716e34a6fd4f171e3cc0ba9eabe70
Leghuir <qwerty1214@gmail.com>
2020-05-03 22:05:25 +0200

    Make program class static

--- WordsTeacher/Program.cs
+++ WordsTeacher/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace WordsTeacher
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
