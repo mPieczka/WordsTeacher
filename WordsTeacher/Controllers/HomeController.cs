96ca5f8cde7b57220f47d168919faac48ed66d8f
Leghuir <qwerty1214@gmail.com>
2020-05-21 22:07:13 +0200

    Add authorization

--- WordsTeacher/Controllers/HomeController.cs
+++ WordsTeacher/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using WordsTeacher.Models;
 
 namespace WordsTeacher.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- WordsTeacher/Controllers/TestController.cs
+++ WordsTeacher/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ using WordsTeacher.Services;
 
 namespace WordsTeacher.Controllers
 {
+    [Authorize]
     public class TestController : Controller
     {
         private readonly TestFactory _testFactory;
