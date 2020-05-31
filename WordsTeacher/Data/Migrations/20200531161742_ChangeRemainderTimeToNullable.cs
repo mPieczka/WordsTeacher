56f391e3f3863af34a49c01cf9a75fd217b2607f
Leghuir <qwerty1214@gmail.com>
2020-05-31 18:18:24 +0200

    Change remainder time to null-able

---
+++ WordsTeacher/Data/Migrations/20200531161742_ChangeRemainderTimeToNullable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WordsTeacher.Data.Migrations
+{
+    public partial class ChangeRemainderTimeToNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RemaiderTimeUtc",
+                table: "Phrases",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RemaiderTimeUtc",
+                table: "Phrases",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
