665990a5aceda1561486a276abe4d8ef9712fa95
Leghuir <qwerty1214@gmail.com>
2020-06-07 22:04:11 +0200

    Add notifications

---
+++ WordsTeacher/Controllers/BaseController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordsTeacher.HtmlHelpers;
+
+namespace WordsTeacher.Controllers
+{
+    public class BaseController : Controller
+    {
+        private List<string> _successMessages;
+        private List<string> _errorMessages;
+        private List<string> _infoMessages;
+        private bool _initlialized;
+
+        public BaseController()
+        {
+            _successMessages = new List<string>();
+            _errorMessages = new List<string>();
+            _infoMessages = new List<string>();
+        }
+
+        private void Initialize()
+        {
+            if (!_initlialized && TempData != null)
+            {
+                TempData[NotificationsName.SuccesNotificationName] = _successMessages;
+                TempData[NotificationsName.ErrorNotificationName] = _errorMessages;
+                TempData[NotificationsName.InformationNotificationName] = _infoMessages;
+                _initlialized = true;
+            }
+        }
+
+        public void AddSuccessMessage(string message)
+        {
+            Initialize();
+            _successMessages.Add(message);
+        }
+
+        public void AddErrorMessage(string message)
+        {
+            Initialize();
+            _errorMessages.Add(message);
+        }
+
+        public void AddInfoMessage(string message)
+        {
+            Initialize();
+            _infoMessages.Add(message);
+        }
+    }
+}
